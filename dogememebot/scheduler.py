90d6eb446bb1b4364412989402a5e8a3489d0dd9
shivam chouhan <shivam.chouhan@intelligentreach.com>
2022-07-31 15:41:56 +0530

    Added scheduler

--- dogememebot/scheduler.py
+++ dogememebot/scheduler.py
@@ -10,7 +10,7 @@ def send_request():
         "spider": "spider"
     })
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=15, minute=45)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=15, minute=46)
 def scheduled_job():
     subprocess.run("scrapyd-deploy", shell=True, universal_newlines=True)
     send_request()
